b74c16e0a0bd2d52b3d78231e57386ba5b00e9fd
Ottenheimer <troyflowrest@gmail.com>
2023-07-21 14:12:03 +0800

    removed static summary

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
-let data = [
-  {
-    "category": "Reaction",
-    "score": 80,
-    "icon": "./assets/images/icon-reaction.svg"
-  },
-  {
-    "category": "Memory",
-    "score": 92,
-    "icon": "./assets/images/icon-memory.svg"
-  },
-  {
-    "category": "Verbal",
-    "score": 61,
-    "icon": "./assets/images/icon-verbal.svg"
-  },
-  {
-    "category": "Visual",
-    "score": 72,
-    "icon": "./assets/images/icon-visual.svg"
+(function() {
+  let data = [
+    {
+      "category": "Reaction",
+      "score": 80,
+      "icon": "./assets/images/icon-reaction.svg"
+    },
+    {
+      "category": "Memory",
+      "score": 92,
+      "icon": "./assets/images/icon-memory.svg"
+    },
+    {
+      "category": "Verbal",
+      "score": 61,
+      "icon": "./assets/images/icon-verbal.svg"
+    },
+    {
+      "category": "Visual",
+      "score": 72,
+      "icon": "./assets/images/icon-visual.svg"
+    }
+  ];
+  
+  const generateSummary = () => {
+    var summaryBreakdown = document.getElementById("summary-breakdown");
+    while(summaryBreakdown.firstChild && summaryBreakdown.removeChild(summaryBreakdown.firstChild));
+  
+    data.forEach( category => {
+      let categoryName = category.category;
+      let categoryNameLower = categoryName.toLowerCase();
+      let card = document.createElement('div');
+      card.setAttribute('class',`summary-card ${categoryNameLower}`);
+  
+      let categoryDiv = document.createElement('div');
+      categoryDiv.setAttribute('class',`${categoryNameLower}-font category`)
+  
+      let image = document.createElement('img');
+      image.setAttribute('src',category.icon);
+      image.setAttribute('alt',categoryNameLower);
+  
+      categoryDiv.appendChild(image);
+  
+      let span = document.createElement('span');
+      span.setAttribute('class','category-name');
+      span.textContent = categoryName;
+  
+      categoryDiv.appendChild(span);
+  
+      card.appendChild(categoryDiv);
+  
+      let scoreDiv = document.createElement('div');
+  
+      let score = document.createElement('p');
+      scoreDiv.textContent = `${category.score} / `;
+  
+      span = document.createElement('span');
+      span.setAttribute('class','light');
+      span.textContent = '100';
+  
+      scoreDiv.appendChild(span);
+  
+      card.appendChild(scoreDiv);
+  
+      summaryBreakdown.appendChild(card);
+  
+  
+    })
   }
-];
-
-const generateSummary = () => {
-  var summaryBreakdown = document.getElementById("summary-breakdown");
-  while(summaryBreakdown.firstChild && summaryBreakdown.removeChild(summaryBreakdown.firstChild));
-
-  data.forEach( category => {
-    let categoryName = category.category;
-    let categoryNameLower = categoryName.toLowerCase();
-    let card = document.createElement('div');
-    card.setAttribute('class',`summary-card ${categoryNameLower}`);
-
-    let categoryDiv = document.createElement('div');
-    categoryDiv.setAttribute('class',`${categoryNameLower}-font category`)
-
-    let image = document.createElement('img');
-    image.setAttribute('src',category.icon);
-    image.setAttribute('alt',categoryNameLower);
-
-    categoryDiv.appendChild(image);
-
-    let span = document.createElement('span');
-    span.setAttribute('class','category-name');
-    span.textContent = categoryName;
-
-    categoryDiv.appendChild(span);
-
-    card.appendChild(categoryDiv);
-
-    let scoreDiv = document.createElement('div');
-
-    let score = document.createElement('p');
-    scoreDiv.textContent = `${category.score} / `;
-
-    span = document.createElement('span');
-    span.setAttribute('class','light');
-    span.textContent = '100';
-
-    scoreDiv.appendChild(span);
-
-    card.appendChild(scoreDiv);
-
-    summaryBreakdown.appendChild(card);
-
-
-  })
-}
-
-generateSummary();
+  
+  generateSummary();
+})();
